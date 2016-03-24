2e97b80c30c9d883112fae525970a09067867140
MatiarIRA <matiar.erfanian@gmail.com>
2016-03-24 13:18:28 -0400

    me

--- app/src/main/java/com/example/louis_edouard/toodle/MainActivity.java
+++ app/src/main/java/com/example/louis_edouard/toodle/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //ok
+    //ok it's me
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
